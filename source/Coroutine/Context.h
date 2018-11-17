2d6dda6aa185147d1303ceff940f4b37f84e0dae
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2018-11-18 01:38:12 +1300

    Simplify implementation and add support for argument.

--- source/Coroutine/Context.h
+++ source/Coroutine/Context.h
@@ -21,38 +21,32 @@ const size_t COROUTINE_REGISTERS = 4;
 typedef struct
 {
 	void **stack_pointer;
-} coroutine_context;
+} CoroutineContext;
 
-typedef COROUTINE(* coroutine_start)(coroutine_context *from, coroutine_context *self) __attribute__((fastcall));
+typedef COROUTINE(* CoroutineStart)(CoroutineContext *from, CoroutineContext *self, void * argument) __attribute__((fastcall));
 
 static inline void coroutine_initialize(
-	coroutine_context *context,
-	coroutine_start start,
+	CoroutineContext *context,
+	CoroutineStart start,
+	void *argument,
 	void *stack_pointer,
 	size_t stack_size
 ) {
-	/* Force 16-byte alignment */
+	/* The i386 System V ABI requires that ESP+4 is 16B-aligned on entry to the function. */
 	context->stack_pointer = (void**)((uintptr_t)stack_pointer & ~0xF);
-
-	if (!start) {
-		assert(!context->stack_pointer);
-		/* We are main coroutine for this thread */
-		return;
-	}
-
+	
+	*--context->stack_pointer = NULL;
+	*--context->stack_pointer = NULL;
+	*--context->stack_pointer = NULL;
+	*--context->stack_pointer = argument;
 	*--context->stack_pointer = NULL;
 	*--context->stack_pointer = (void*)start;
-
+	
 	context->stack_pointer -= COROUTINE_REGISTERS;
 	memset(context->stack_pointer, 0, sizeof(void*) * COROUTINE_REGISTERS);
 }
 
-coroutine_context * coroutine_transfer(coroutine_context * current, coroutine_context * target) __attribute__((fastcall));
-
-static inline void coroutine_destroy(coroutine_context * context)
-{
-	context->stack_pointer = NULL;
-}
+CoroutineContext * coroutine_transfer(CoroutineContext * current, CoroutineContext * target) __attribute__((fastcall));
 
 #if __cplusplus
 }
